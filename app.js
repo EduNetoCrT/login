6be1f1d9d06f3d7f753ab6db47ac3d995babcb92
Eduardo Neto <eduardo.js_jp@hotmail.com>
2022-06-29 15:22:07 -0300

    primeiro commit com arquivo de servidor

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => console.log(`Server in port ${process.env.PORT}`));
